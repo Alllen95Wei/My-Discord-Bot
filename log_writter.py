46c10ffe7db6c24c2122ae9bd278415a30dd822c
Allen95Wei(901) <allen95wei@gmail.com>
2022-01-04 16:07:09 +0800

    log記錄改以`utf-8`編碼

--- log_writter.py
+++ log_writter.py
@@ -8,7 +8,7 @@ def write_log(content, no_time_stamp=False):
         time_stamp = time.strftime("%Y-%m-%d %p %I:%M:%S", local_time)
         log = "[" + time_stamp + "]" + log
     try:
-        log_file = open("log.txt", mode="a")
+        log_file = open("log.txt", mode="a", encoding="utf-8")
         log_file.write(log)
         log_file.close()
     except Exception as e:
