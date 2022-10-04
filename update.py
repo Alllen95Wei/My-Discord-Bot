39acdbb834d139be6c6592501bb2a9e0d46e96c9
Allen98637 <asfasfasf986375@gmail.com>
2022-10-04 10:49:52 +0800

    update.py 修正`subprocess.run`的指令格式為清單

--- update.py
+++ update.py
@@ -2,12 +2,12 @@ def update(pid, os):
     import subprocess
     from time import sleep
 
-    subprocess.run("git fetch --all")
-    subprocess.run("git reset --hard origin/main")
-    subprocess.run("git pull")
+    subprocess.run(["git", "fetch", "--all"])
+    subprocess.run(['git', 'reset', '--hard', 'origin/main'])
+    subprocess.run(['git', 'pull'])
     sleep(5)
     subprocess.Popen("python main.py", creationflags=subprocess.CREATE_NEW_CONSOLE)
     if os == "Windows":
-        subprocess.run("taskkill /f /PID {0}".format(pid))
+        subprocess.run(['taskkill', '/f', '/PID', str(format(pid))])
     elif os == "Linux":
-        subprocess.run("kill -9 {0}".format(pid))
+        subprocess.run(['kill', '-9', str(format(pid))])
