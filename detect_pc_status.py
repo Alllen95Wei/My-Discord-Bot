a2dda11690eb90295c7356cf44c009e40e43d017
Allen98637 <allen95wei@gmail.com>
2022-10-27 08:04:10 +0800

    detect_pc_status.py 1. 修正參數引用 2. 回傳記憶體實際容量

--- detect_pc_status.py
+++ detect_pc_status.py
@@ -1,11 +1,18 @@
 def pc_status():
     try:
         import psutil
-        msg = "CPU使用率：" + str(psutil.cpu_percent()) + "%" + " / 記憶體使用率：" + str(psutil.virtual_memory().percent) + "%"
+        cpu_usage = psutil.cpu_percent()
+        ram_usage = psutil.virtual_memory().percent
+        ram_total = round(psutil.virtual_memory().total / (1024^3))
+        ram_free = round(psutil.virtual_memory().free / (1024^3))
+        msg = "CPU使用率：" + str(cpu_usage) + "%" + " / 記憶體使用率：" + str(ram_usage) + "%" + "`(" + str(ram_free) + "MB / " + str(ram_total) + "MB)`"
     except Exception as e:
         msg = "```" + str(e) + "```"
     return msg
 
 
 if __name__ == "__main__":
-    print(pc_status())
+    from time import sleep
+    while True:
+        print(pc_status())
+        sleep(1)
