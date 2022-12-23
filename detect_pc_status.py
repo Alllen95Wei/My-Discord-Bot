627991982443c799746bf3cae23613a3499e9e8f
Allen Wei <allen95wei@gmail.com>
2022-12-23 12:01:28 +0800

    detect_pc_status.py 美化程式碼 requirements.txt 更新所需模組

--- detect_pc_status.py
+++ detect_pc_status.py
@@ -3,9 +3,10 @@ def pc_status():
         import psutil
         cpu_usage = psutil.cpu_percent()
         ram_usage = psutil.virtual_memory().percent
-        ram_total = round(psutil.virtual_memory().total / (1024^3))
-        ram_free = round(psutil.virtual_memory().free / (1024^3))
-        msg = "CPU使用率：" + str(cpu_usage) + "%" + " / 記憶體使用率：" + str(ram_usage) + "%" + "`(" + str(ram_free) + "MB / " + str(ram_total) + "MB)`"
+        ram_total = round(psutil.virtual_memory().total / (1024 ^ 3))
+        ram_free = round(psutil.virtual_memory().free / (1024 ^ 3))
+        msg = "CPU使用率：" + str(cpu_usage) + "%" + " / 記憶體使用率：" + str(ram_usage) + "%" + "`(" + str(
+            ram_free) + "MB / " + str(ram_total) + "MB)`"
     except Exception as e:
         msg = "```" + str(e) + "```"
     return msg
@@ -13,6 +14,7 @@ def pc_status():
 
 if __name__ == "__main__":
     from time import sleep
+
     while True:
         print(pc_status())
         sleep(1)
